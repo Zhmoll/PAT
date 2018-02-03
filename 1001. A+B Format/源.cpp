69058c0f1930648df6de946bd2a5627d6af48f42
张效伟 <zhmoll@zhmoll.com>
2018-02-03 20:22:58 +0800

    开始Zhmoll的PAT

---
+++ 1001. A+B Format/源.cpp
@@ -0,0 +1,19 @@
+#include <stdio.h>
+
+int main() {
+	int a = 0, b = 0;
+	int sum = 0;
+	scanf("", &a, &b);
+	sum = a + b;
+
+	if (sum < 0) {
+		sum = -sum;
+		printf("-");
+	}
+
+	int num = 0;
+
+	while (sum != 0) {
+		num = sum / 1000
+	}
+}
